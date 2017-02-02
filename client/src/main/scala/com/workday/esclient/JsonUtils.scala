a9a2ef4fabeebaa977d011117eb3b793d32b233e
skeletalbassman <steven.helferich@gmail.com>
2017-02-01 16:11:19 -0800

    removing comments

--- client/src/main/scala/com/workday/esclient/JsonUtils.scala
+++ client/src/main/scala/com/workday/esclient/JsonUtils.scala
@@ -1,8 +1,5 @@
 package com.workday.esclient
 
-/**
-  * Created by steven.helferich on 2/1/17.
-  */
 //TODO: remove this and replace with circe
 object JsonUtils {
   def equals(lhs: String, rhs: String): Boolean = {
