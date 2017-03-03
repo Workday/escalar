bac302cddbe3e432c35e2953efdc620a6955e78f
skeletalbassman <steven.helferich@gmail.com>
2017-03-03 15:31:06 -0800

    refactoring project structure

--- project/Build.scala
+++ project/Build.scala
@@ -3,19 +3,30 @@ import sbt._
 import scoverage.{ScoverageKeys, ScoverageSbtPlugin}
 
 object Build extends Build {
-  val EsClientVersion = "latest.integration"
 
-  lazy val root = Project(id = "scala-elasticsearch-client-root", base = file("."))
+  lazy val comonSettings = Seq(
+    organization := "com.workday",
+    version := "0.1.0-SNAPSHOT",
+    scalaVersion := "2.10.4"
+  )
+
+  lazy val root = Project(id = "escalar", base = file("."))
     .settings(
-      scalaVersion := "2.10.4",
+      comonSettings,
       libraryDependencies ++= Seq(
+        "io.searchbox" % "jest" % "0.1.7",
+        "org.mockito" % "mockito-core" % "1.9.5",
+        "org.scalatest" %% "scalatest" % "2.1.2" % "test",
         "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.5.3",
         "com.fasterxml.jackson.core" % "jackson-annotations" % "2.5.3",
         "com.fasterxml.jackson.core" % "jackson-databind" % "2.5.3",
         "com.fasterxml.jackson.core" % "jackson-core" % "2.5.3"
-      )
-    ) aggregate(client)
-
+      ),
+      ScoverageKeys.coverageEnabled := true,
+      ScoverageKeys.coverageMinimum := 100,
+      ScoverageKeys.coverageFailOnMinimum := true
+    )
+/*
   lazy val client = Project(id = "client", base = file("client"))
     .settings(
       name := "client",
@@ -31,5 +42,7 @@ object Build extends Build {
       ScoverageKeys.coverageEnabled := true,
       ScoverageKeys.coverageMinimum := 100,
       ScoverageKeys.coverageFailOnMinimum := true
+
     )
+    */
 }
